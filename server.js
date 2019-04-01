b6ebb4807cd88b206f44715d811eb9ef5502d7fd
Carlos Vinícius Monteiro Costa <cvmcosta@yahoo.com.br>
2019-04-01 17:34:45 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+//Projeto para a turma de Redes 2
+
+
+//socket library
+const socket = require ("socket.io")
+//mandatory love-you library
+const love = require ("love-you")
+
+console.log(love.random());
